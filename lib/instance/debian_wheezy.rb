53a8f08a1b170ec4c3dc61df407fbb3f369692ac
Jezra <jezra@jezra.net>
2014-05-09 09:16:27 -0700

    Updated debian_wheezy.rb to reference an updated AMI

--- lib/instance/debian_wheezy.rb
+++ lib/instance/debian_wheezy.rb
@@ -6,7 +6,7 @@ class Nano::DebianWheezy < Aether::Instance::Default
 
   # Debian AMIs are listed at https://wiki.debian.org/Cloud/AmazonEC2Image/Wheezy
   self.default_options = {
-    :image_id => "ami-0da18864",
+    :image_id => "ami-b7c8d5de",
     :user_data => proc { Base64.encode64(Aether::Instance.user_file(:cloud_init, 'debian_wheezy_puppet.yaml').read) }
   }
 end
